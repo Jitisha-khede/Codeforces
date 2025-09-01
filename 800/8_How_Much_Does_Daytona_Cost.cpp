8cfe9bc97b34be35c874ed9769e4022309fee180
Jitisha Khede <khedejitisha2004@gmail.com>
2025-09-01 22:12:50 +0530

    accepted

---
+++ 800/8_How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        string x,s;
+        vector<int> a(n);
+        bool found = 0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]==k){
+                found = 1;
+                break;
+            }
+        }
+        if(found==1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
