fa772ca57457a6c27fc9a9cf8080d02bbe5698a9
Jitisha Khede <khedejitisha2004@gmail.com>
2025-09-02 12:01:07 +0530

    wrong answer

---
+++ 800/11_Ambitious_Kid.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        int ans = 0;
+        for(int i=0;i<n-1;i++){
+            ans = min(ans,abs(a[i]));
+        }
+        cout<<ans<<endl;
+  
+    return 0;
+}
