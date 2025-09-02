e11a212b8f69936ed22882eebf54bd1950501652
Jitisha Khede <khedejitisha2004@gmail.com>
2025-09-02 13:25:40 +0530

    Accepted

--- 800/11_Ambitious_Kid.cpp
+++ 800/11_Ambitious_Kid.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 int main()
 {
-    
         int n;
         cin>>n;
-        vector<int> a(n);
+        vector<long long> a(n);
         for(int i=0;i<n;i++){
             cin>>a[i];
         }
-        sort(a.begin(),a.end());
-        int ans = 0;
-        for(int i=0;i<n-1;i++){
+        long long ans = INT_MAX;
+        for(int i=0;i<n;i++){
             ans = min(ans,abs(a[i]));
         }
         cout<<ans<<endl;
